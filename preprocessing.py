8b9bb227ab085c3035f264b43a7a5f7f889b5a83
Benjamin Fiske <bffiske@gmail.com>
2021-11-22 15:15:04 -0500

    filtered jokes

--- preprocessing.py
+++ preprocessing.py
@@ -1,23 +1,34 @@
+from numpy.core.arrayprint import format_float_scientific
 import pandas as pd
 
 # should be a lowercase list of banned words
 banned_list = []
+with open("badwords.txt", "r") as f:
+    banned_list = f.read().splitlines()
+
 jokes = pd.read_csv('shortjokes.csv')['Joke'].tolist()
 
 filtered = []
-for raw_joke in jokes:
+for i, raw_joke in enumerate(jokes):
+    if i % 10000 == 0:
+        print(i)
     # check if any innaproriate words are contained within the joke
-    for t in raw_joke.lower():
-        if t in banned_list:
-            continue
-    if len(raw_joke) > 400:
-        continue
-    filtered.append(raw_joke)
+    bad = False
+    for t in banned_list:
+        if t in raw_joke.lower():
+            bad=True
+    if len(raw_joke) < 400 and not bad:
+        filtered.append(raw_joke)
 
 with open("filtered_jokes.txt", "w") as file:
     for j in filtered:
         file.write(j + "\n")
 
+## strip punctuation
+## save ?, ..., *, -, :, " as separate words
+the_jokes = []
+with open("filtered_jokes.txt", "w") as file:
+    the_jokes = file.read().splitlines()
 
 
 
